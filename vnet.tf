09387eda7e0100d2501ea413047dff2bcb8943ad
William Patton <wjpatton@wjpatton.com>
2020-04-01 14:48:12 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "test" {
+  name     = "william-workshop"
+  location = "CentralUS"
+}
+
 module "network" {
   source  = "app.terraform.io/hc-william-demo/network/azurerm"
   version = "3.0.1"
